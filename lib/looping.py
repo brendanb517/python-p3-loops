17587bcfd71188e00acabf20268349f3a96390dd
Brendan <brendan.b517@gmail.com>
2023-04-10 13:57:39 -0400

    Passed all tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    num = 10
+    while num > 0:
+        print(f"{num}")
+        num -= 1
+    if num == 0:
+        print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squares_list = [num ** 2 for num in int_list]
+    return squares_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if (i % 3 == 0 and i % 5 == 0):
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
